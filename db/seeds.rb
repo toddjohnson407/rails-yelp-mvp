8442a8ff2991e5b1d665bb6320571c2632d4ebeb
Todd Johnson <toddjohnson407@gmail.com>
2018-05-23 04:26:16 -0600

    db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurant_attributes = [
+  {
+    name: 'McDonalds',
+    address: 'Everywhere',
+    phone_number: '3241234',
+    category: 'italian'
+  },
+  {
+    name: 'Super Mario',
+    address: 'Lisboa',
+    phone_number: '39302',
+    category: 'french'
+  },
+  {
+    name: "Swen's Food",
+    address: "Swen's House",
+    phone_number: '93939',
+    category: 'japanese'
+  },
+  {
+    name: "Diogo's Restaurant",
+    address: "His House",
+    phone_number: "12",
+    category: "belgian"
+  },
+  {
+    name: "Tony's Restaurant",
+    address: "Tony's Home",
+    phone_number: "5555555",
+    category: "chinese"
+  }
+]
+
+Restaurant.create!(restaurant_attributes)
+
+puts 'finished'
+
+
+
+
+
+
+
+
+
+
